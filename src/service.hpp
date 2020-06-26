baf5c0f42b4528bc26c9e541febcca7d14dfaff0
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-06-27 00:21:32 +0200

    service: add a simple http server

--- src/service.hpp
+++ src/service.hpp
@@ -7,6 +7,7 @@
 #include <net/ipv4/tcp.hpp>
 #include <net/dispatch.hpp>
 #include <async/basic.hpp>
+#include <frg/string.hpp>
 #include <lib/logger.hpp>
 #include <net/arp.hpp>
 
@@ -44,18 +45,10 @@ struct service {
 		auto &tcp_proc = ed_.nth_processor<0>().nth_processor<2>();
 		tcp_proc.set_arp_processor(&ed_.nth_processor<1>());
 
-		auto sock = co_await tcp_proc.listen(1337);
-
-		lib::log("tart: %u.%u.%u.%u connected (ports: in %u, out %u)\r\n",
-				sock->ip()[0], sock->ip()[1],
-				sock->ip()[2], sock->ip()[3],
-				sock->in_port(), sock->out_port());
-		//while (true) {
-		auto b = co_await sock->recv();
-		lib::log("tart: received '%.*s'\r\n", b.size() - 1, b.data());
-		co_await sock->send(b.data(), b.size());
-		co_await sock->close();
-		//}
+		while (true) {
+			auto sock = co_await tcp_proc.listen(80);
+			connection_worker(sock);
+		}
 	}
 
 private:
@@ -72,6 +65,45 @@ private:
 		>,
 		net::arp_processor
 	> ed_;
+
+	async::detached connection_worker(auto sock) {
+		lib::log("tart: %u.%u.%u.%u connected (ports: in %u, out %u)\r\n",
+				sock->ip()[0], sock->ip()[1],
+				sock->ip()[2], sock->ip()[3],
+				sock->in_port(), sock->out_port());
+
+		while (sock->connected()) {
+			auto b = co_await sock->recv();
+
+			frg::string_view req{static_cast<char *>(b.data()), b.size()};
+			size_t newl = req.find_first('\n');
+			auto http_req = req.sub_string(0, newl);
+
+			auto s1 = http_req.find_first(' ');
+			auto s2 = http_req.find_last(' ');
+
+			if (s1 == size_t(-1) || s2 == size_t(-1))
+				continue;
+
+			auto type = http_req.sub_string(0, s1);
+			auto path = http_req.sub_string(s1 + 1, s2 - s1 - 1);
+
+			lib::log("tart: got a http request!, type \"%.*s\", path \"%.*s\"\r\n", type.size(), type.data(), path.size(), path.data());
+
+			frg::string_view resp;
+			if (type != "GET")
+				resp = "HTTP/1.1 400 Bad request\nServer: tart\nContent-Type: text/html\nContent-Length: 20\n\nBad request ...what?";
+			if (path == "/" || path == "/index.html")
+				resp = "HTTP/1.1 200 OK\nServer: tart\nContent-Type: text/html\nContent-Length: 101\n\nHello, world! This web page was served to you from <a href=\"https://github.com/qookei/tart\">tart</a>!";
+			else if (path == "/other.html")
+				resp = "HTTP/1.1 200 OK\nServer: tart\nContent-Type: text/html\nContent-Length: 80\n\nHello, world ...again! This is a different page. <a href=\"index.html\">Go back</a>";
+			else
+				resp = "HTTP/1.1 404 File not found\nServer: tart\nContent-Type: text/html\nContent-Length: 38\n\nI don't know what you're asking for...";
+
+			co_await sock->send(const_cast<char *>(resp.data()), resp.size());
+		}
+		co_await sock->close();
+	}
 };
 
 } // namespace service
