dd6582af649f6ba11594ff63544cacf108d7dbf7
Kacper Słomiński <kacper.slominski72@gmail.com>
2021-11-01 14:31:14 +0100

    lib: drop clang support for now

--- src/cxx-support.cpp
+++ src/cxx-support.cpp
@@ -45,27 +45,3 @@ void operator delete(void *p, size_t size){
 void operator delete[](void *p, size_t size){
 	tart::alloc().deallocate(p, size);
 }
-
-#ifdef CLANG_MEMES
-extern "C" {
-	[[gnu::used]] void *__aeabi_memclr(void *dest, size_t size) {
-		return memset(dest, 0, size);
-	}
-
-	[[gnu::used]] void *__aeabi_memclr4(void *dest, size_t size) {
-		return memset(dest, 0, size);
-	}
-
-	[[gnu::used]] void *__aeabi_memclr8(void *dest, size_t size) {
-		return memset(dest, 0, size);
-	}
-
-	[[gnu::used]] void *__aeabi_memcpy(void *dest, const void *src, size_t size) {
-		return memcpy(dest, src, size);
-	}
-
-	[[gnu::used]] void *__aeabi_memcpy4(void *dest, const void *src, size_t size) {
-		return memcpy(dest, src, size);
-	}
-}
-#endif
