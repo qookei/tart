ad50faadf0bc7fdeda388fb30f8a34b6fac53bcb
Kacper Słomiński <kacper.slominski72@gmail.com>
2022-10-25 22:51:10 +0200

    drivers: clk: use int64_t for clock frequencies

--- include/tart/drivers/clk.hpp
+++ include/tart/drivers/clk.hpp
@@ -7,7 +7,7 @@ namespace tart {
 		clk(clk *parent = nullptr)
 		: parent_clk_{parent} { }
 
-		virtual uint32_t freq() const = 0;
+		virtual int64_t freq() const = 0;
 		virtual void start() = 0;
 		virtual void stop() = 0;
 
@@ -21,14 +21,22 @@ namespace tart {
 	};
 
 	struct fixed_clk final : clk {
-		fixed_clk(uint32_t freq)
+		fixed_clk(int64_t freq)
 		: clk{nullptr}, freq_{freq} { }
 
-		uint32_t freq() const override { return freq_; }
+		int64_t freq() const override { return freq_; }
 		void start() override { }
 		void stop() override { }
 
 	private:
-		uint32_t freq_;
+		int64_t freq_;
 	};
+
+	constexpr uint64_t operator""_KHz(unsigned long long freq) {
+		return freq * 1000ULL;
+	}
+
+	constexpr uint64_t operator""_MHz(unsigned long long freq) {
+		return freq * 1000000ULL;
+	}
 } // namespace tart
--- include/tart/drivers/clk/rp2-osc.hpp
+++ include/tart/drivers/clk/rp2-osc.hpp
@@ -6,19 +6,19 @@
 namespace tart {
 
 struct rp2_xosc final : clk {
-	rp2_xosc(uintptr_t base, uint32_t freq)
+	rp2_xosc(uintptr_t base, int64_t freq)
 	: clk{nullptr}, freq_{freq}, space_{base} { }
 
 	virtual ~rp2_xosc() = default;
 
-	uint32_t freq() const override { return freq_; }
+	int64_t freq() const override { return freq_; }
 	void start() override;
 	void stop() override;
 
 	void busy_wait(uint8_t cycles);
 
 private:
-	uint32_t freq_;
+	int64_t freq_;
 
 	// TODO(qookie): Remove the :: prefix once tart::arch is gone.
 	::arch::mem_space space_;
